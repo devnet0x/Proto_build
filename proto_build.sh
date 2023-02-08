c7bd5b9e4d489133a9a1fe3e5d1d1b0db78fdca6
devnet0x <devnet0x@gmail.com>
2023-02-08 15:53:07 -0300

    Add testnet2

--- proto_build.sh
+++ proto_build.sh
@@ -7,9 +7,9 @@
 ######################################################
 #!/bin/bash
 
-if [[ ($# -lt 3) || ($1 != "devnet" && $1 != "testnet") ]]
+if [[ ($# -lt 3) || ($1 != "devnet" && $1 != "testnet" && $1 != "testnet2") ]]
 then
-   echo "Usage: proto_build.sh <devnet|testnet> <key_file> <json_compiled_file> [constructor parameters]"
+   echo "Usage: proto_build.sh <devnet|testnet|testnet2> <key_file> <json_compiled_file> [constructor parameters]"
    exit
 fi
 
@@ -18,12 +18,21 @@ if [ $1 == "devnet" ]
 then
    ENVIRONMENT1="--gateway-url http://127.0.0.1:5050 --chain-id=1536727068981429685321"
    ENVIRONMENT2="--feeder_gateway_url http://127.0.0.1:5050"
+   ENVIRONMENT_TESTNET2=""
 fi
 
 if [ $1 == "testnet" ]
 then
    ENVIRONMENT1="--network testnet"
    ENVIRONMENT2="--network alpha-goerli"
+   ENVIRONMENT_TESTNET2=""
+fi
+
+if [ $1 == "testnet2" ]
+then
+   ENVIRONMENT1=""
+   ENVIRONMENT2="--network alpha-goerli2"
+   ENVIRONMENT_TESTNET2="-p testnet2"
 fi
 
 # Set keys parameters
@@ -54,6 +63,7 @@ fi
 
 FILENAME=`basename $3 .json`
 CLASS_HASH=`cat build.tmp | grep ${FILENAME} | awk 'FNR == 1 {print $6}'`
+
 ###################################################
 #                                                 #
 # Declare                                         #
@@ -61,7 +71,7 @@ CLASS_HASH=`cat build.tmp | grep ${FILENAME} | awk 'FNR == 1 {print $6}'`
 ###################################################
 
 echo -e "\033[1;32mDeclaring...\033[0m"
-DECLARE_STATEMENT="protostar declare ${3} ${ENVIRONMENT1} --account-address ${PUBLIC_KEY} --max-fee auto > build.tmp"
+DECLARE_STATEMENT="protostar ${ENVIRONMENT_TESTNET2} declare ${3} ${ENVIRONMENT1} --account-address ${PUBLIC_KEY} --max-fee auto > build.tmp"
 echo ${DECLARE_STATEMENT}
 eval ${DECLARE_STATEMENT}
 if [ $? -ne 0 ]
@@ -70,7 +80,7 @@ then
    exit
 fi
 
-if [ $1 == "devnet" ]
+if [[ ($1 == "devnet") || ($1 == "testnet2") ]]
 then
    TX_HASH=`cat build.tmp | awk 'FNR == 3 {print $3}'`
 else
@@ -89,7 +99,7 @@ do
    TX_STATUS=`starknet tx_status ${ENVIRONMENT2} --hash ${TX_HASH} | awk 'FNR == 2 {print $2}'`
 done
 TX_STATUS=`starknet tx_status ${ENVIRONMENT2} --hash ${TX_HASH} | awk 'FNR == 3 {print $2}'`
-echo ${TX_STATUS} "    "
+echo -ne "${TX_STATUS} $(( SECONDS - start )) secs.\n"
 if [ ${TX_STATUS} != "\"ACCEPTED_ON_L2\"" ]
 then
    cat build.tmp
@@ -104,7 +114,7 @@ fi
 ###################################################
 
 echo -e "\033[1;32mDeploying...\033[0m"
-DEPLOY_STATEMENT="protostar deploy ${CLASS_HASH} ${ENVIRONMENT1} --max-fee auto --account-address ${PUBLIC_KEY} ${INPUTS} > build.tmp"
+DEPLOY_STATEMENT="protostar ${ENVIRONMENT_TESTNET2} deploy ${CLASS_HASH} ${ENVIRONMENT1} --max-fee auto --account-address ${PUBLIC_KEY} ${INPUTS} > build.tmp"
 echo ${DEPLOY_STATEMENT}
 eval ${DEPLOY_STATEMENT}
 if [ $? -ne 0 ]
@@ -115,7 +125,7 @@ fi
 
 CONTRACT_ADDRESS=`cat build.tmp | awk 'FNR == 2 {print $3}'`
 
-if [ $1 == "devnet" ]
+if [[ ($1 == "devnet") || ($1 == "testnet2") ]]
 then
    TX_HASH=`cat build.tmp | awk 'FNR == 3 {print $3}'`
 else
@@ -139,7 +149,7 @@ do
 done
 TX_STATUS=`starknet tx_status ${ENVIRONMENT2} --hash ${TX_HASH} | awk 'FNR == 3 {print $2}'`
 
-echo ${TX_STATUS} "    "
+echo -ne "${TX_STATUS} $(( SECONDS - start )) secs.\n"
 if [ ${TX_STATUS} != "\"ACCEPTED_ON_L2\"" ]
 then
    cat build.tmp
